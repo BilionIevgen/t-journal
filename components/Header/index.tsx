749d9fe5466ded3329f0ce1a2b51f433b36b88ff
BilionIevgeniy <bilionjenia1111@gmail.com>
2022-05-31 22:41:54 +0300

    change prettier settings

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import {
-    Paper,
-    Button,
-    IconButton,
-    Avatar,
-    List,
-    ListItem,
-    Box,
+  Paper,
+  Button,
+  IconButton,
+  Avatar,
+  List,
+  ListItem,
+  Box,
 } from "@material-ui/core";
 import Image from "next/image";
 import {
-    SearchOutlined as SearchIcon,
-    SmsOutlined as MessageIcon,
-    Menu as MenuIcon,
-    ExpandMoreOutlined as ArrowBottom,
-    NotificationsNoneOutlined as NotificationIcon,
-    AccountCircleOutlined as UserIcon,
+  SearchOutlined as SearchIcon,
+  SmsOutlined as MessageIcon,
+  Menu as MenuIcon,
+  ExpandMoreOutlined as ArrowBottom,
+  NotificationsNoneOutlined as NotificationIcon,
+  AccountCircleOutlined as UserIcon,
 } from "@material-ui/icons";
 import logo from "../../public/img/logo.svg";
 import styles from "./Header.module.scss";
@@ -27,61 +27,61 @@ import styles from "./Header.module.scss";
 // import { Api } from '../../utils/api';
 
 export const Header: React.FC = () => {
-    //   const userData = useAppSelector(selectUserData);
-    //   const [authVisible, setAuthVisible] = React.useState(false);
-    const [searchValue, setSearchValue] = React.useState("");
-    //   const [posts, setPosts] = React.useState<PostItem[]>([]);
+  //   const userData = useAppSelector(selectUserData);
+  //   const [authVisible, setAuthVisible] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
+  //   const [posts, setPosts] = React.useState<PostItem[]>([]);
 
-    //   const openAuthDialog = () => {
-    //     setAuthVisible(true);
-    //   };
+  //   const openAuthDialog = () => {
+  //     setAuthVisible(true);
+  //   };
 
-    //   const closeAuthDialog = () => {
-    //     setAuthVisible(false);
-    //   };
+  //   const closeAuthDialog = () => {
+  //     setAuthVisible(false);
+  //   };
 
-    //   React.useEffect(() => {
-    //     if (authVisible && userData) {
-    //       setAuthVisible(false);
-    //     }
-    //   }, [authVisible, userData]);
+  //   React.useEffect(() => {
+  //     if (authVisible && userData) {
+  //       setAuthVisible(false);
+  //     }
+  //   }, [authVisible, userData]);
 
-    const handleChangeInput = async (e) => {
-        setSearchValue(e.target.value);
-        try {
-            //   const { items } = await Api().post.search({ title: e.target.value });
-            //   setPosts(items);
-        } catch (e) {
-            console.warn(e);
-        }
-    };
+  const handleChangeInput = async (e) => {
+    setSearchValue(e.target.value);
+    try {
+      //   const { items } = await Api().post.search({ title: e.target.value });
+      //   setPosts(items);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
 
-    return (
-        <Paper classes={{ root: styles.root }} elevation={0}>
-            <Box className="d-flex align-center">
-                <IconButton>
-                    <MenuIcon />
-                </IconButton>
-                <Link href="/">
-                    <a>
-                        <Image
-                            height={35}
-                            width={100}
-                            className="mr-20"
-                            src={logo}
-                            alt="Logo"
-                        />
-                    </a>
-                </Link>
+  return (
+    <Paper classes={{ root: styles.root }} elevation={0}>
+      <Box className="d-flex align-center">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <Link href="/">
+          <a>
+            <Image
+              height={35}
+              width={100}
+              className="mr-20"
+              src={logo}
+              alt="Logo"
+            />
+          </a>
+        </Link>
 
-                <Box className={styles.searchBlock}>
-                    <SearchIcon />
-                    <input
-                        value={searchValue}
-                        onChange={handleChangeInput}
-                        placeholder="Поиск"
-                    />
-                    {/* {posts.length > 0 && (
+        <Box className={styles.searchBlock}>
+          <SearchIcon />
+          <input
+            value={searchValue}
+            onChange={handleChangeInput}
+            placeholder="Поиск"
+          />
+          {/* {posts.length > 0 && (
             <Paper className={styles.searchBlockPopup}>
               <List>
                 {posts.map((obj) => (
@@ -94,33 +94,30 @@ export const Header: React.FC = () => {
               </List>
             </Paper>
           )} */}
-                </Box>
+        </Box>
 
-                <Link href="/write">
-                    <a>
-                        <Button
-                            variant="contained"
-                            className={styles.penButton}
-                        >
-                            Новая запись
-                        </Button>
-                    </a>
-                </Link>
-            </Box>
-            <Box className="d-flex align-center">
-                <IconButton>
-                    <MessageIcon />
-                </IconButton>
-                <IconButton>
-                    <NotificationIcon />
-                </IconButton>
-                <Avatar
-                    className={styles.avatar}
-                    alt="Remy Sharp"
-                    src="https://leonardo.osnova.io/5ffeac9a-a0e5-5be6-98af-659bfaabd2a6/-/scale_crop/108x108/-/format/webp/"
-                />
-                <ArrowBottom />
-                {/* {userData ? (
+        <Link href="/write">
+          <a>
+            <Button variant="contained" className={styles.penButton}>
+              Новая запись
+            </Button>
+          </a>
+        </Link>
+      </Box>
+      <Box className="d-flex align-center">
+        <IconButton>
+          <MessageIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationIcon />
+        </IconButton>
+        <Avatar
+          className={styles.avatar}
+          alt="Remy Sharp"
+          src="https://leonardo.osnova.io/5ffeac9a-a0e5-5be6-98af-659bfaabd2a6/-/scale_crop/108x108/-/format/webp/"
+        />
+
+        {/* {userData ? (
           <Link href="/profile/1">
             <a className="d-flex align-center">
               <Avatar
@@ -137,8 +134,8 @@ export const Header: React.FC = () => {
             Войти
           </Box>
         )} */}
-            </Box>
-            {/* <AuthDialog onClose={closeAuthDialog} visible={authVisible} /> */}
-        </Paper>
-    );
+      </Box>
+      {/* <AuthDialog onClose={closeAuthDialog} visible={authVisible} /> */}
+    </Paper>
+  );
 };
